30c3def1ed46583bc075ef726b6915b099f6f728
yunus <altuntasyunus50@gmail.com>
2021-07-18 15:20:51 +0300

    authentication

--- app.py
+++ app.py
@@ -1,30 +1,40 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
+from flask_jwt import JWT, jwt_required
+
+from security import authenticate, identity
 
 app = Flask(__name__)
+app.secret_key = 'jose'
 api = Api(app)
+
+jwt = JWT(app, authenticate, identity)
 
 items = []
 
+
 class Item(Resource):
+    @jwt_required()
     def get(self, name):
-        item=next(filter(lamda x:x["name"]==name,items),None)
-        return {"item": None},200 if item is not None else 404
-    def post(self,name):
-        if next(filter(lambda  x:x["name"]==name,items),None) is not None:
-            return {"message":"An item with name '{}' already exist ".format(name)},400
+        item = next(filter(lambda x: x["name"] == name, items), None)
+        return {"item": item}, 200 if item is not None else 404
+
+    def post(self, name):
+        if next(filter(lambda x: x["name"] == name, items), None) is not None:
+            return {"message": "An item with name '{}' already exist ".format(name)}, 400
         data = request.get_json(force=True)
         print(data)
         item = {'name': name, "price": 12.00}
         items.append(item)
-        return item,201
+        return item, 201
 
 
 class ItemList(Resource):
     def get(self):
-        return  {"items": items}
+        return {"items": items}
 
-api.add_resource(ItemList,'/items')
+
+api.add_resource(ItemList, '/items')
 api.add_resource(Item, '/item/<string:name>')
 
 app.run(debug=True)
---
+++ security.py
@@ -0,0 +1,19 @@
+from user import User
+from werkzeug.security import safe_str_cmp
+
+users = [
+    User(1, 'bob', 'asdf')
+]
+username_mapping = {u.username: u for u in users}
+userid_mapping = {u.id: u for u in users}
+
+
+def authenticate(username, password):
+    user = username_mapping.get(username, None)
+    if user and user.password == password:
+        return user
+
+
+def identity(payload):
+    userid = payload['identity']
+    return userid_mapping.get(userid, None)

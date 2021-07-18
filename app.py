cd9a64cc32acdecdd6a9a33ded5214f28928b7bc
yunus <altuntasyunus50@gmail.com>
2021-07-18 22:54:48 +0300

    add required place

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 from flask_jwt import JWT, jwt_required
 
 from security import authenticate, identity
@@ -34,11 +34,18 @@ class Item(Resource):
         return {"message": "Item deleted"}
 
     def put(self, name):
-        data = request.get_json()
+        parser = reqparse.RequestParser()
+        parser.add_argument('price',
+                            type=float,
+                            required=True,
+                            help='This field cannot be left blank!'
+                            )
+        data = parser.parse_args()
         item = next(filter(lambda x: x["name"] == name, items), None)
+        print(item)
         if item is None:
             item = {'name': name, 'price': data["price"]}
-            items.append(data)
+            items.append(item)
         else:
             item.update(data)
         return item

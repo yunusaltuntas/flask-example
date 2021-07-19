3543fef8d3ac77acfd8db06ba73ea7fca5a5591d
yunus <altuntasyunus50@gmail.com>
2021-07-19 14:33:42 +0300

    item api connected db

--- create_table.py
+++ create_table.py
@@ -3,9 +3,12 @@ import sqlite3
 connection = sqlite3.connect("data.db")
 curser=connection.cursor()
 
-create_table = "CREATE TABLE  IF NOT EXISTS users (id INTEGER PRIMARY KEY , username text, password text)"
+create_table = "CREATE TABLE  IF NOT EXISTS users (id INTEGER PRIMARY KEY , username text, password real)"
 curser.execute(create_table)
 
+create_table = "CREATE TABLE  IF NOT EXISTS items (name text, price real)"
+curser.execute(create_table)
+curser.execute("INSERT INTO items VALUES ('test',10.99)")
 connection.commit()
 
 connection.close()

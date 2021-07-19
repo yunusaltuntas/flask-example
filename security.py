21b025541ec14d5ef86f2a063a6a3012717f07da
yunus <altuntasyunus50@gmail.com>
2021-07-19 12:05:45 +0300

    sql database added

--- security.py
+++ security.py
@@ -1,19 +1,13 @@
 from user import User
-from werkzeug.security import safe_str_cmp
-
-users = [
-    User(1, 'bob', 'asdf')
-]
-username_mapping = {u.username: u for u in users}
-userid_mapping = {u.id: u for u in users}
+# from werkzeug.security import safe_str_cmp
 
 
 def authenticate(username, password):
-    user = username_mapping.get(username, None)
+    user = User.find_by_username(username)
     if user and user.password == password:
         return user
 
 
 def identity(payload):
-    userid = payload['identity']
-    return userid_mapping.get(userid, None)
+    user_id = payload['identity']
+    return User.find_by_id(user_id)

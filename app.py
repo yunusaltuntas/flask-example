c19e2f51e3656dd603578de707336464db08246a
yunus <altuntasyunus50@gmail.com>
2021-07-23 22:23:18 +0300

    code refactoring

--- app.py
+++ app.py
@@ -14,10 +14,13 @@ api = Api(app)
 
 jwt = JWT(app, authenticate, identity)
 
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
 db.init_app(app)
 api.add_resource(ItemList, '/items')
 api.add_resource(Item, '/item/<string:name>')
 api.add_resource(UserRegister, '/register')
 if __name__=='__main__':
-
     app.run(debug=True)
--- model/user.py
+++ model/user.py
@@ -5,12 +5,11 @@ from db import db
 class UserModel(db.Model):
     __tablename__ = 'users'
 
-    id = db.Column(db.Integer,primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     username =db.Column(db.String(80))
     password = db.Column(db.String(80))
 
-    def __init__(self, _id, username, password):
-        self.id = _id
+    def __init__(self, username, password):
         self.username = username
         self.password = password
 
@@ -23,6 +22,6 @@ class UserModel(db.Model):
         return cls.query.filter_by(id=_id).first()
 
     def save_to_db(self):
-        db.session.save(self)
+        db.session.add(self)
         db.session.commit()
 
